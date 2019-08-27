c02ebe68ff905ba57141482f43704433ccf818f0
Alec Loudenback <alecloudenback@gmail.com>
2019-08-26 21:46:57 -0500

    comment out documenter for now

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,8 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, ActSci2   
 
-makedocs(sitename="Actuarial Science v2 (WIP)")
+# makedocs(sitename="Actuarial Science v2 (WIP)")
 
-deploydocs(
-    repo = "github.com/alecloudenback/ActSci2.jl.git",
-)
+# deploydocs(
+#     repo = "github.com/alecloudenback/ActSci2.jl.git",
+# )
